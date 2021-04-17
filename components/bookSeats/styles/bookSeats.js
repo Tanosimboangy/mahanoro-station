d9ea518c53f1edaf700881c86e62eab6f161dc9a
Tanosimboangy <jacquit.aja@onja.org>
2021-04-17 11:29:52 +0300

    adding some styles

--- components/bookSeats/styles/bookSeats.js
+++ components/bookSeats/styles/bookSeats.js
@@ -1,147 +1,154 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Container = styled.div`
-    max-width: 1114px;
-    margin-right: auto;
-    margin-left: auto;
-    padding-left: 32px;
-    padding-right: 32px;
-    display: grid;
+  max-width: 1114px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 32px;
+  padding-right: 32px;
+  display: grid;
 
-    @media(min-width: 1114px) {
-        grid-template-columns: auto auto;
-        column-gap: 72px;
-    }
-`;
-const Frame = styled.div``;
+  @media (min-width: 900px) {
+    grid-template-columns: auto auto;
+    column-gap: 72px;
+  }
+`
+const Frame = styled.div`
+  margin-bottom: 30px;
+`
 const Subtitle = styled.h2`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 28px;
-    letter-spacing: 0.05em;
-    text-transform: uppercase;
-    color: #0F0E17;
-`;
-const Image = styled.img``;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  color: #0f0e17;
+  margin-bottom: 40px;
+`
+const Image = styled.img``
 const InfoContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-`;
+  display: flex;
+  justify-content: space-between;
+`
 const Info = styled.span`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 28px;
-    color: #000000;
-    :first-of-type {
-        color: #A7A9BE;
-    }
-`;
-const BookingContainer = styled.div``;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  color: #000000;
+  :first-of-type {
+    color: #a7a9be;
+  }
+`
+const BookingContainer = styled.div``
 
 const SeatsContainer = styled.div`
-    display: grid;
-    grid-template-columns: 60px 60px 60px 60px;
-    grid-template-areas: "blank blank image1 image2"
-                          "image3 image4 image5 image6"
-                          "image7 image8 image9 image10"
-                          "image11 image12 image13 image14"
-                          "image15 image16 image17 image18";
+  display: grid;
+  grid-template-columns: 60px 60px 60px 60px;
+  grid-template-areas:
+    'blank blank image1 image2'
+    'image3 image4 image5 image6'
+    'image7 image8 image9 image10'
+    'image11 image12 image13 image14'
+    'image15 image16 image17 image18';
 
-    img:nth-of-type(1) {
-        grid-area: image1; 
-    }
+  img:nth-of-type(1) {
+    grid-area: image1;
+  }
 
-    img:nth-of-type(2) {
-        grid-area: image2;
-    }
-     img:nth-of-type(3) {
-        grid-area: image3;
-    }
-     img:nth-of-type(4) {
-        grid-area: image4;
-    }
-     img:nth-of-type(5) {
-        grid-area: image5;
-    }
-     img:nth-of-type(6) {
-        grid-area: image6;
-    }
-     img:nth-of-type(7) {
-        grid-area: image7;
-    }
-     img:nth-of-type(8) {
-        grid-area: image8;
-    }
-     img:nth-of-type(9) {
-        grid-area: image9;
-    }
-     img:nth-of-type(10) {
-        grid-area: image10;
-    }
-     img:nth-of-type(11) {
-        grid-area: image11;
-    }
-     img:nth-of-type(12) {
-        grid-area: image12;
-    }
-     img:nth-of-type(13) {
-        grid-area: image13;
-    }
-     img:nth-of-type(14) {
-        grid-area: image14;
-    }
-     img:nth-of-type(15) {
-        grid-area: image15;
-    }
-     img:nth-of-type(16) {
-        grid-area: image16;
-    }
-    img:nth-of-type(17) {
-        grid-area: image17;
-    }
-    img:nth-of-type(18) {
-        grid-area: image18;
-    }
-`;
+  img:nth-of-type(2) {
+    grid-area: image2;
+  }
+  img:nth-of-type(3) {
+    grid-area: image3;
+  }
+  img:nth-of-type(4) {
+    grid-area: image4;
+  }
+  img:nth-of-type(5) {
+    grid-area: image5;
+  }
+  img:nth-of-type(6) {
+    grid-area: image6;
+  }
+  img:nth-of-type(7) {
+    grid-area: image7;
+  }
+  img:nth-of-type(8) {
+    grid-area: image8;
+  }
+  img:nth-of-type(9) {
+    grid-area: image9;
+  }
+  img:nth-of-type(10) {
+    grid-area: image10;
+  }
+  img:nth-of-type(11) {
+    grid-area: image11;
+  }
+  img:nth-of-type(12) {
+    grid-area: image12;
+  }
+  img:nth-of-type(13) {
+    grid-area: image13;
+  }
+  img:nth-of-type(14) {
+    grid-area: image14;
+  }
+  img:nth-of-type(15) {
+    grid-area: image15;
+  }
+  img:nth-of-type(16) {
+    grid-area: image16;
+  }
+  img:nth-of-type(17) {
+    grid-area: image17;
+  }
+  img:nth-of-type(18) {
+    grid-area: image18;
+  }
+`
 
 const Price = styled.p`
-    font-weight: 500;
-    font-size: 48px;
-    line-height: 57px;
-    color: #FF8906;
-`;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  color: #ff8906;
+  margin-top: 20px;
+`
 
 const BookingButton = styled.button`
-    background: #E53170;
-    font-weight: normal;
-    font-size: 24px;
-    line-height: 28px;
-    text-align: center;
-    color: #FFFFFF;
-    padding-top: 22px;
-    padding-bottom: 22px;
-    padding-left: 50px;
-    padding-right: 50px;
-`;
+  background: #e53170;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+  color: #ffffff;
+  padding-top: 22px;
+  padding-bottom: 22px;
+  padding-left: 50px;
+  padding-right: 50px;
+  outline: none;
+  border: none;
+`
 const TotalPrice = styled.p`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 28px;
-    letter-spacing: 0.05em;
-    text-transform: uppercase;
-    color: #000000
-`;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  color: #000000;
+`
 
 export {
-    Container,
-    Frame,
-    Subtitle,
-    SeatsContainer,
-    Image,
-    InfoContainer,
-    Info,
-    BookingContainer,
-    Price,
-    BookingButton,
-    TotalPrice
+  Container,
+  Frame,
+  Subtitle,
+  SeatsContainer,
+  Image,
+  InfoContainer,
+  Info,
+  BookingContainer,
+  Price,
+  BookingButton,
+  TotalPrice,
 }
--- dist/Mahanoro-station-project.e31bb0bc.js
+++ dist/Mahanoro-station-project.e31bb0bc.js
@@ -39877,142 +39877,149 @@ var _styledComponents = _interopRequireDefault(require("styled-components"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const Container = _styledComponents.default.div`
-    max-width: 1114px;
-    margin-right: auto;
-    margin-left: auto;
-    padding-left: 32px;
-    padding-right: 32px;
-    display: grid;
+  max-width: 1114px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 32px;
+  padding-right: 32px;
+  display: grid;
 
-    @media(min-width: 1114px) {
-        grid-template-columns: auto auto;
-        column-gap: 72px;
-    }
+  @media (min-width: 900px) {
+    grid-template-columns: auto auto;
+    column-gap: 72px;
+  }
 `;
 exports.Container = Container;
-const Frame = _styledComponents.default.div``;
+const Frame = _styledComponents.default.div`
+  margin-bottom: 30px;
+`;
 exports.Frame = Frame;
 const Subtitle = _styledComponents.default.h2`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 28px;
-    letter-spacing: 0.05em;
-    text-transform: uppercase;
-    color: #0F0E17;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  color: #0f0e17;
+  margin-bottom: 40px;
 `;
 exports.Subtitle = Subtitle;
 const Image = _styledComponents.default.img``;
 exports.Image = Image;
 const InfoContainer = _styledComponents.default.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `;
 exports.InfoContainer = InfoContainer;
 const Info = _styledComponents.default.span`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 28px;
-    color: #000000;
-    :first-of-type {
-        color: #A7A9BE;
-    }
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  color: #000000;
+  :first-of-type {
+    color: #a7a9be;
+  }
 `;
 exports.Info = Info;
 const BookingContainer = _styledComponents.default.div``;
 exports.BookingContainer = BookingContainer;
 const SeatsContainer = _styledComponents.default.div`
-    display: grid;
-    grid-template-columns: 60px 60px 60px 60px;
-    grid-template-areas: "blank blank image1 image2"
-                          "image3 image4 image5 image6"
-                          "image7 image8 image9 image10"
-                          "image11 image12 image13 image14"
-                          "image15 image16 image17 image18";
+  display: grid;
+  grid-template-columns: 60px 60px 60px 60px;
+  grid-template-areas:
+    'blank blank image1 image2'
+    'image3 image4 image5 image6'
+    'image7 image8 image9 image10'
+    'image11 image12 image13 image14'
+    'image15 image16 image17 image18';
 
-    img:nth-of-type(1) {
-        grid-area: image1; 
-    }
+  img:nth-of-type(1) {
+    grid-area: image1;
+  }
 
-    img:nth-of-type(2) {
-        grid-area: image2;
-    }
-     img:nth-of-type(3) {
-        grid-area: image3;
-    }
-     img:nth-of-type(4) {
-        grid-area: image4;
-    }
-     img:nth-of-type(5) {
-        grid-area: image5;
-    }
-     img:nth-of-type(6) {
-        grid-area: image6;
-    }
-     img:nth-of-type(7) {
-        grid-area: image7;
-    }
-     img:nth-of-type(8) {
-        grid-area: image8;
-    }
-     img:nth-of-type(9) {
-        grid-area: image9;
-    }
-     img:nth-of-type(10) {
-        grid-area: image10;
-    }
-     img:nth-of-type(11) {
-        grid-area: image11;
-    }
-     img:nth-of-type(12) {
-        grid-area: image12;
-    }
-     img:nth-of-type(13) {
-        grid-area: image13;
-    }
-     img:nth-of-type(14) {
-        grid-area: image14;
-    }
-     img:nth-of-type(15) {
-        grid-area: image15;
-    }
-     img:nth-of-type(16) {
-        grid-area: image16;
-    }
-    img:nth-of-type(17) {
-        grid-area: image17;
-    }
-    img:nth-of-type(18) {
-        grid-area: image18;
-    }
+  img:nth-of-type(2) {
+    grid-area: image2;
+  }
+  img:nth-of-type(3) {
+    grid-area: image3;
+  }
+  img:nth-of-type(4) {
+    grid-area: image4;
+  }
+  img:nth-of-type(5) {
+    grid-area: image5;
+  }
+  img:nth-of-type(6) {
+    grid-area: image6;
+  }
+  img:nth-of-type(7) {
+    grid-area: image7;
+  }
+  img:nth-of-type(8) {
+    grid-area: image8;
+  }
+  img:nth-of-type(9) {
+    grid-area: image9;
+  }
+  img:nth-of-type(10) {
+    grid-area: image10;
+  }
+  img:nth-of-type(11) {
+    grid-area: image11;
+  }
+  img:nth-of-type(12) {
+    grid-area: image12;
+  }
+  img:nth-of-type(13) {
+    grid-area: image13;
+  }
+  img:nth-of-type(14) {
+    grid-area: image14;
+  }
+  img:nth-of-type(15) {
+    grid-area: image15;
+  }
+  img:nth-of-type(16) {
+    grid-area: image16;
+  }
+  img:nth-of-type(17) {
+    grid-area: image17;
+  }
+  img:nth-of-type(18) {
+    grid-area: image18;
+  }
 `;
 exports.SeatsContainer = SeatsContainer;
 const Price = _styledComponents.default.p`
-    font-weight: 500;
-    font-size: 48px;
-    line-height: 57px;
-    color: #FF8906;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  color: #ff8906;
+  margin-top: 20px;
 `;
 exports.Price = Price;
 const BookingButton = _styledComponents.default.button`
-    background: #E53170;
-    font-weight: normal;
-    font-size: 24px;
-    line-height: 28px;
-    text-align: center;
-    color: #FFFFFF;
-    padding-top: 22px;
-    padding-bottom: 22px;
-    padding-left: 50px;
-    padding-right: 50px;
+  background: #e53170;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  text-align: center;
+  color: #ffffff;
+  padding-top: 22px;
+  padding-bottom: 22px;
+  padding-left: 50px;
+  padding-right: 50px;
+  outline: none;
+  border: none;
 `;
 exports.BookingButton = BookingButton;
 const TotalPrice = _styledComponents.default.p`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 28px;
-    letter-spacing: 0.05em;
-    text-transform: uppercase;
-    color: #000000
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: 0.05em;
+  text-transform: uppercase;
+  color: #000000;
 `;
 exports.TotalPrice = TotalPrice;
 },{"styled-components":"node_modules/styled-components/dist/styled-components.browser.esm.js"}],"components/bookSeats/index.js":[function(require,module,exports) {
